9b017b15328c6f12f454f59ce2239ce282b1dc3a
ufukomer <ufukomer@gmail.com>
2016-02-07 22:07:49 +0200

    ES6 Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,43 @@
-"use strict";
+const gulp = require('gulp');
+const thrift = require('gulp-thrift');
 
-var gulp = require('gulp');
-var thrift = require('gulp-thrift');
+const version = 'chd5-2.1_5.3.0/';
 
-var version = 'chd5-2.1_5.3.0/';
-
-var basePaths = {
-  src: 'src/',
-  dest: 'lib/'
+const basePaths = {
+    src: 'src/',
+    dest: 'lib/'
 };
 
-var paths = {
-  js: {
-    src: basePaths.src + 'impala.js',
-    dest: basePaths.dest
-  },
-  thrift: {
-    src: basePaths.src + version + '*.thrift',
-    dest: basePaths.dest + version
-  }
+const paths = {
+    js: {
+        src: basePaths.src + 'impala.js',
+        dest: basePaths.dest
+    },
+    thrift: {
+        src: basePaths.src + version + '*.thrift',
+        dest: basePaths.dest + version
+    }
 };
 
 // 'gulp' command runs watch task as default
 gulp.task('default', ['watch']);
 
 gulp.task('watch', ['js'], () => {
-  // Watch changes on impala.js
-  gulp.watch(paths.js.src, ['js']);
+    // Watch changes on impala.js
+    gulp.watch(paths.js.src, ['js']);
 });
 
 // Generate thrift files
 gulp.task('thrift', () => {
-  return gulp.src(paths.thrift.src)
-    .pipe(thrift({gen:'js:node'}))
-    .pipe(gulp.dest(paths.thrift.dest));
+    gulp.src(paths.thrift.src)
+        .pipe(thrift({
+            gen: 'js:node'
+        }))
+        .pipe(gulp.dest(paths.thrift.dest));
 });
 
 // Copy impla.js from src/ to lib/
 gulp.task('js', () => {
-  return gulp.src(paths.js.src)
-    .pipe(gulp.dest(paths.js.dest));
+    gulp.src(paths.js.src)
+        .pipe(gulp.dest(paths.js.dest));
 });
